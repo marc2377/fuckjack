cfb97eb430183179bc9bb9de5fa648913aff1d33
McFy49 <31405855+McFy49@users.noreply.github.com>
2017-11-09 11:11:08 +0100

    Update auto-fuckjack.sh
    for my usage -> 6gb of ram

--- auto-fuckjack.sh
+++ auto-fuckjack.sh
@@ -15,9 +15,9 @@
 # IMPORTANT! IMPORTANT! IMPORTANT! IMPORTANT! IMPORTANT! IMPORTANT! IMPORTANT! IMPORTANT! IMPORTANT! IMPORTANT!
 #
 # Replace the value in the first of "ANDROID_JACK_VM_ARGS" with half of your RAM
-# (i.e.: if you have 8GB of ram, use "-Xmx4g")
+# (i.e.: if you have 6GB of ram, use "-Xmx3g")
 #
-  export ANDROID_JACK_VM_ARGS="-Xmx4g -XX:+TieredCompilation -Dfile.encoding=UTF-8"
+  export ANDROID_JACK_VM_ARGS="-Xmx3g -XX:+TieredCompilation -Dfile.encoding=UTF-8"
 #
 # IMPORTANT! IMPORTANT! IMPORTANT! IMPORTANT! IMPORTANT! IMPORTANT! IMPORTANT! IMPORTANT! IMPORTANT! IMPORTANT!
 
